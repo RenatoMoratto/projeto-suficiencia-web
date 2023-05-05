79653653eb1c059298b51679a8d726c0f3e26fc3
Renato Willyan Moratto Filho <renatowmoratto@gmail.com>
2023-05-05 17:03:11 -0300

    build(frontend): add https protocol

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	define: {
 		"process.env": process.env,
+	},
+	server: {
+		https: {
+			key: fs.readFileSync("./.cert/key.pem"),
+			cert: fs.readFileSync("./.cert/cert.pem"),
+		},
 	},
 });
