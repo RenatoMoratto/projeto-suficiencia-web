00b67c3c344d9c0ff915b29a0e3c9265303e8ec1
Renato Willyan Moratto Filho <renatowmoratto@gmail.com>
2023-05-07 19:58:36 -0300

    style(backend): organize environment variables

--- backend/user-api/src/constants/environment_vars.js
+++ backend/user-api/src/constants/environment_vars.js
@@ -1,24 +1,24 @@
 const {
-	DB_USER,
-	DB_PASSWORD,
+	DB_READ_USER,
+	DB_READ_PASS,
 	DB_HOST,
 	DB_PORT,
 	DB_NAME,
-	RABBIT_HOST,
-	RABBIT_PORT,
-	RABBIT_USER,
-	RABBIT_PASSWORD,
-	NODE_DOCKER_PORT,
+	MQ_HOST,
+	MQ_PORT,
+	MQ_USER,
+	MQ_PASS,
+	API_DOCKER_PORT,
 	JWT_SECRET,
 	REDIS_HOST,
 	REDIS_PORT,
 } = process.env;
 
-export const db_connection_string = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
-export const port = NODE_DOCKER_PORT;
+export const db_connection_string = `mongodb://${DB_READ_USER}:${DB_READ_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
+export const port = API_DOCKER_PORT;
 export const jwt_secret = JWT_SECRET;
 export const redis_connection_string = `redis://${REDIS_HOST}:${REDIS_PORT}`;
-export const rabbit_host = RABBIT_HOST;
-export const rabbit_port = RABBIT_PORT;
-export const rabbit_user = RABBIT_USER;
-export const rabbit_password = RABBIT_PASSWORD;
+export const rabbit_host = MQ_HOST;
+export const rabbit_port = MQ_PORT;
+export const rabbit_user = MQ_USER;
+export const rabbit_password = MQ_PASS;
--- backend/user-queue/src/constants/environment_vars.js
+++ backend/user-queue/src/constants/environment_vars.js
@@ -1,21 +1,21 @@
 const {
-	DB_USER,
-	DB_PASSWORD,
+	DB_WRITE_USER,
+	DB_WRITE_PASS,
 	DB_HOST,
 	DB_PORT,
 	DB_NAME,
-	RABBIT_HOST,
-	RABBIT_PORT,
-	RABBIT_USER,
-	RABBIT_PASSWORD,
+	MQ_HOST,
+	MQ_PORT,
+	MQ_USER,
+	MQ_PASS,
 	DEFAULT_USER,
-	PORT,
+	QUEUE_DOCKER_PORT,
 } = process.env;
 
-export const db_connection_string = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
-export const rabbit_host = RABBIT_HOST;
-export const rabbit_port = RABBIT_PORT;
-export const rabbit_user = RABBIT_USER;
-export const rabbit_password = RABBIT_PASSWORD;
-export const port = PORT;
+export const db_connection_string = `mongodb://${DB_WRITE_USER}:${DB_WRITE_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
+export const rabbit_host = MQ_HOST;
+export const rabbit_port = MQ_PORT;
+export const rabbit_user = MQ_USER;
+export const rabbit_password = MQ_PASS;
+export const port = QUEUE_DOCKER_PORT;
 export const default_user = JSON.parse(DEFAULT_USER);
